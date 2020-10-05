9bad52084ea5ea5829542ce84ecfeae3ad4d137c
faithkatherine <faithcathy12@gmail.com>
2020-10-05 20:19:03 +0300

    part3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
   albumInfo;
-  constructor(_productService:ProductService){}
+  constructor(private _productService:ProductService){}
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
